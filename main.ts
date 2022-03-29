0bdffb35eaaacfa0362800f883ac733512fbea09
Nekoshita Yuki <>
2022-03-29 15:21:29 +0900

    chore: install eslint and add lint command to package.json

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { Plugin, Notice } from 'obsidian';
 
 export default class ObsidianOGP extends Plugin {
     async onload() {
-        const ribbonIconEl = this.addRibbonIcon('dice', 'Obsidian OGP', (evt: MouseEvent) => {
+        const ribbonIconEl = this.addRibbonIcon('dice', 'Obsidian OGP', () => {
 			new Notice('Hello world!');
 		});
         ribbonIconEl.addClass('my-plugin-ribbon-class');
