06f442cdea2512556d7694222b3c31812d3ce0b7
Nekoshita Yuki <>
2022-03-30 13:08:38 +0900

    refactor: rename this repository "obsidian-plugin-template"

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 import { Plugin, Notice } from 'obsidian';
 
-export default class ObsidianOGP extends Plugin {
+export default class ObsidianPluginTemplate extends Plugin {
     async onload() {
-        const ribbonIconEl = this.addRibbonIcon('dice', 'Obsidian OGP', () => {
+        const ribbonIconEl = this.addRibbonIcon('dice', 'Obsidian Plugin Template', () => {
 			new Notice('Hello world!');
 		});
         ribbonIconEl.addClass('my-plugin-ribbon-class');
